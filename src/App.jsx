77f21f8800629e96367129affcde459b456bfb29
Rafee Jenkins <rafeejenkins@gmail.com>
2023-02-14 19:56:51 +0000

    feat: adds loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { db } from '../config/firebaseConfig'
-import { addDoc, collection, deleteDoc, doc, updateDoc } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, orderBy, query, updateDoc } from 'firebase/firestore'
 import CommentBox from './components/CommentBox'
 import { Button, Card, CircularProgress, Input } from '@mui/material'
 
@@ -10,8 +10,9 @@ function App() {
   const [comment, setComment] = useState(0)
 
   const [comments, loading, error] = useCollection(
+    query(
     collection(
-      db, 'comments')
+      db, 'comments'), orderBy('timestamp', 'desc'))
   )
 
   const handleSubmit = (e) => {
@@ -42,7 +43,7 @@ function App() {
       <Card style={{ padding: 5, borderRadius: 5, margin: 5 }}>
         <h2 style={{ color: 'black' }}>Comments</h2>
         <form style={{ display: 'flex', justifyContent: 'center' }}>
-          <Input type="text" onChange={handleInput} placeholder="Enter your comment here" />
+          <Input type="text" onChange={handleInput} placeholder="Enter your comment here" value={comment}/>
           <Button variant="contained" onClick={handleSubmit}>Submit</Button>
         </form>
       </Card>
--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -4,7 +4,7 @@ import IconButton from '@mui/material/IconButton';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Card } from '@mui/material';
+import { Card, CircularProgress } from '@mui/material';
 
 export default function CommentBox({ handleLike, handleDelete, comment, score, liked, idx }) {
     const [color, setColor] = useState('black')
@@ -32,9 +32,9 @@ export default function CommentBox({ handleLike, handleDelete, comment, score, l
     }
 
     return (
-        <Card key={idx} style={{ margin: 10, backgroundColor: color, borderRadius: 5, padding: 2, display: 'flex', flexDirection: 'column' }}>
+        <Card key={idx} style={{ margin: 10, backgroundColor: color, borderRadius: 5, padding: 2, display: 'flex', flexDirection: 'column', justifyItems: 'center' }}>
             <div style={{ color: 'white' }}>{comment}</div>
-            {score && <div style={{ color: 'white' }}>Google Sentiment Score: {score}</div>}
+            {score ? <div style={{ color: 'white' }}>Google Sentiment Score: {score}</div> : <CircularProgress style={{ alignSelf: 'center'}}/>}
             <div style={{ backgroundColor: 'white', display: 'flex', justifyContent: 'center', borderRadius: 3 }}>
                 <IconButton aria-label="delete" color="white" onClick={handleDelete}>
                     <DeleteIcon />
