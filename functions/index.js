384b20dee1176046cc2c0ebc2e39a3eda10ae738
Rafee Jenkins <rafeejenkins@gmail.com>
2023-02-14 19:17:03 +0000

    feat: tidies UI

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require("firebase-functions");
 const {LanguageServiceClient} = require("@google-cloud/language");
 
-exports.analyseComment = functions.
-    firestore.document("comments/{commentId}").onCreate((snap, context) => {
+exports.analyseComment = functions.region("europe-west2").
+    firestore.document("comments/{commentId}")
+    .onCreate((snap, context) => {
       const {comment} = snap.data();
 
       const client = new LanguageServiceClient();
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { db } from '../config/firebaseConfig'
-import { addDoc, collection, query, orderBy, deleteDoc, doc, updateDoc } from 'firebase/firestore'
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { addDoc, collection, deleteDoc, doc, updateDoc } from 'firebase/firestore'
+import CommentBox from './components/CommentBox'
+import { Button, Card, CircularProgress, Input } from '@mui/material'
 
 function App() {
   const [comment, setComment] = useState(0)
@@ -34,40 +31,39 @@ function App() {
 
   const handleLike = (event, id) => {
     console.log(event.target.checked, id)
-    updateDoc(doc(db, `comments/${id}`), { liked: event.target.checked})
+    updateDoc(doc(db, `comments/${id}`), { liked: event.target.checked })
   }
 
-  const getColor = (score) => {
-    if (!score) {
-      return 'black'
-    }
-    return score > 0 ? 'green' : 'red'
-  }
+
+
 
   return (
     <div className="App">
-      <form>
-        <input type="text" onChange={handleInput} />
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-      </form>
+      <Card style={{ padding: 5, borderRadius: 5, margin: 5 }}>
+        <h2 style={{ color: 'black' }}>Comments</h2>
+        <form style={{ display: 'flex', justifyContent: 'center' }}>
+          <Input type="text" onChange={handleInput} placeholder="Enter your comment here" />
+          <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+        </form>
+      </Card>
       <div>
-        {loading && <span>Collection: Loading...</span>}
+        {loading && <CircularProgress />}
         {comments && (
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            {comments.docs.map((doc, idx) => {
-              const { comment, score, liked } = doc.data();
-              const color = getColor(score)
-              return (
-              <div key={idx} style={{ margin: 10, backgroundColor: color, borderRadius: 5, padding: 2 }}>
-                <div style={{ color: 'white' }}>{comment}</div>
-                { score && <div style={{ color: 'white' }}>{score}</div>}
-                <IconButton aria-label="delete" color="white" onClick={() => handleDelete(doc.id)}>
-        <DeleteIcon />
-      </IconButton>
-                <Checkbox checked={liked} onChange={(e) => handleLike(e, doc.id)} icon={<FavoriteBorder />} checkedIcon={<Favorite />}/>
-              </div>
-              )
-              })}
+            {
+              comments.docs.map((doc, idx) => {
+                const { comment, score, liked } = doc.data();
+                return (
+                  <CommentBox
+                    score={score}
+                    key={idx}
+                    comment={comment}
+                    liked={liked}
+                    handleDelete={() => handleDelete(doc.id)}
+                    handleLike={(e) => handleLike(e, doc.id)} />
+                )
+              })
+            }
           </div>
         )}
       </div>
