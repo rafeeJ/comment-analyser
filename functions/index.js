d7e34ab748dac9adae8ffecbb376adac3e58bf52
Rafee Jenkins <rafeejenkins@gmail.com>
2023-02-17 18:16:16 +0000

    feat: updates todos

--- functions/index.js
+++ functions/index.js
@@ -4,22 +4,5 @@ const {LanguageServiceClient} = require("@google-cloud/language");
 exports.analyseComment = functions.region("europe-west2").
     firestore.document("comments/{commentId}")
     .onCreate((snap, context) => {
-      const {comment} = snap.data();
-
-      const client = new LanguageServiceClient();
-      const request = {
-        document: {
-          content: comment,
-          type: "PLAIN_TEXT",
-        },
-        encodingType: "UTF8",
-      };
-
-      client.analyzeSentiment(request).then((results) => {
-        const {documentSentiment} = results[0];
-        const {score, magnitude} = documentSentiment;
-        return snap.ref.set({score, magnitude}, {merge: true});
-      }).catch((err) => {
-        console.error("ERROR:", err);
-      });
+      // TO-DO: analyse the comment
     });
